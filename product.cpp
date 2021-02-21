dbabdfd2e858f1c2c917e72c1fe310ed3abed4e1
vardhanrajya08 <66107299+vardhanrajya08@users.noreply.github.com>
2021-02-21 22:27:00 +0530

    created product.cpp
    After here entering two numbers 3.5 & 5.5 it must give an output 18.7

---
+++ product.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a, b, c;
+    cout<<"Enter two numbers:";
+    cin>>a;
+    cin>>b;
+    c=a*b;
+    cout<<"Product = "<<c;
+    return 0;
+}
