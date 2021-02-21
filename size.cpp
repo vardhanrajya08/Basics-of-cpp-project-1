5e12c412b4897d5f911e2b5a275f3a965dc27dd2
vardhanrajya08 <66107299+vardhanrajya08@users.noreply.github.com>
2021-02-21 22:21:37 +0530

    Created size.cpp
    This prints Size of char: 1 byte Size of int: 4 bytes Size of float: 4 bytes Size of double: 8 bytes

---
+++ size.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Size of char: "<<sizeof(char)<<" byte ";
+    cout<<"Size of int: "<<sizeof(int)<<" bytes ";
+    cout<<"Size of float: "<<sizeof(float)<<" bytes ";
+    cout<<"Size of double: "<<sizeof(double)<<" Bytes";
+    return 0;
+}
