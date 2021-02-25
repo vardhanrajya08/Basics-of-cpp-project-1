ad958e1279f8296d55dcf94c846d309dfe2306b6
vardhanrajya08 <66107299+vardhanrajya08@users.noreply.github.com>
2021-02-26 00:49:37 +0530

    Create vote eligible 3.cpp
    C++ program where we check if a person is eligible to vote or not. Version 3

---
+++ vote eligible 3.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+
+    int age;
+    cout << "Enter age of a user:";
+    cin>>age;
+
+    if (age<0)
+    {
+        cout<<"Enter valid age";
+    }
+    else
+    {
+        switch (age)
+        {
+            case 1:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 2:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 3:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 4:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 5:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 6:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 7:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 8:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 9:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 10:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 11:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 12:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 13:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 14:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 15:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 16:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            case 17:
+            cout<<"You are not eligible for voting";
+            break;
+            
+            default :
+            cout<<"You are eligible for vote";
+        }
+    }
+    return 0;
+}
