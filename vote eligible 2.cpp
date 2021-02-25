27065630023c188dfebb67778f3a2161f8f39e9f
vardhanrajya08 <66107299+vardhanrajya08@users.noreply.github.com>
2021-02-26 00:29:57 +0530

    Create vote eligible 2.cpp
    C++ program where we check if a person is eligible to vote or not. version 2

---
+++ vote eligible 2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+
+    int age;
+    cout << "Enter age of a user:";
+    cin>>age;
+
+    if (age > 17) 
+    {
+        cout << "You are eligible for voting";
+    } 
+    else 
+    {
+        cout << "You are not eligible for voting";
+    }
+
+    return 0;
+}
